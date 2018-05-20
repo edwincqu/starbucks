f8d428a5d936e075c40ef99b3fa04756640cab08
Samuel <davrv93@gmail.com>
2018-05-20 13:46:00 +0000

    Urls para produccion

--- rewards/urls.py
+++ rewards/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
 from . import views
-
-
-urlpatterns = [
+try:
+    from django.urls import include, path
+    urlpatterns = [
 	path('', views.IndexView.as_view(), name="index"),
 	path('createuser/', views.UserCreationView.as_view(), name="create-user"),
-]
+    ]
+
+except Exception as e:
+    from django.conf.urls import url, include
+    urlpatterns = [
+	url('', views.IndexView.as_view(), name="index"),
+	url('createuser/', views.UserCreationView.as_view(), name="create-user"),
+    ]
+
+    pass
+#from django.urls import path
+
+
+
+
+#urlpatterns = [
+#	path('', views.IndexView.as_view(), name="index"),
+#	path('createuser/', views.UserCreationView.as_view(), name="create-user"),
+#]
