3850697652493caa1dd3e2bd6c4333f233e38404
Samuel <samuelroncal@24dcaea7d349>
2018-05-20 05:24:06 +0000

    Configuracion de produccion

--- starbucks/urls.py
+++ starbucks/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+try:
+    from django.urls import path
+except Exception as e:
+    from django.conf.urls import url
+    pass
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+try:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ]
+except Exception as e:
+    urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    ]
+    pass
