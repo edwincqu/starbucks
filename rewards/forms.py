0a2d0fe1b85baded2b4a15f852456c5e289bf5f4
Edwin Calsin Quinto <edwincqu@gmail.com>
2018-05-20 06:48:59 -0500

    udpate form

--- rewards/forms.py
+++ rewards/forms.py
@@ -13,6 +13,7 @@ class UserCreateForm(UserCreationForm):
 	receive_news = forms.BooleanField()
 
 
+
 	class Meta:
 		model = User
 		fields = (
@@ -29,10 +30,27 @@ class UserCreateForm(UserCreationForm):
 			"pin_code",
 			"accept_terms",
 			"receive_news")
+		widgets = {
+			"username":forms.TextInput(attrs={'class':'form-control'}),
+			"first_name":forms.TextInput(attrs={'class':'form-control'}),
+			"last_name":forms.TextInput(attrs={'class':'form-control'}),
+			"email":forms.TextInput(attrs={'class':'form-control'}),
+			"password1":forms.PasswordInput(attrs={'class':'form-control'}),
+			"password2":forms.PasswordInput(attrs={'class':'form-control'}),
+			"question":forms.TextInput(attrs={'class':'custom-select custom-select-sm'}),
+			"answer":forms.TextInput(attrs={'class':'form-control'}),
+			"card_number":forms.TextInput(attrs={'class':'form-control'}),
+			"pin_code":forms.TextInput(attrs={'class':'form-control'}),
+			"accept_terms":forms.TextInput(attrs={'class':'form-check-input"'}),
+			"receive_news":forms.TextInput(attrs={'class':'form-check-input"'})
+
+		}  
 
 	def save(self, commit=True):
+		print('guardo')
 		user = super(UserCreateForm, self).save()
 		if user:
+			print('user ...>', user)
 			instance = Person(
 				user=user,
 				birthdate = self.cleaned_data['birthdate'],
